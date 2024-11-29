a9bb552545f5a0d0db0bdb915e0dd7fe0a5a0ce5
Jayawickrama Y.R.C.S <chamodansajeewana@gmail.com>
2024-11-29 22:13:57 +0530

    imported webgazer

--- src/Components/TicTacToe/LandingPage.jsx
+++ src/Components/TicTacToe/LandingPage.jsx
@@ -1,6 +1,31 @@
 import Sidebar from './Sidebar'
 import { Outlet } from 'react-router-dom'
+import { useEffect, useState } from 'react'
 const LandingPage = () => {
+  const [isVideoOn, setIsVideoOn] = useState(false)
+  const [gazeDataCollection, setGazeDataCollection] = useState([])
+  const webgazer = window.webgazer
+
+  useEffect(() => {
+    const listener = (data, elapsedTime) => {
+      if (data) {
+        setGazeDataCollection((prev) => [
+          ...prev,
+          { x: data.x, y: data.y, time: elapsedTime / 1000 },
+        ])
+      }
+    }
+
+    webgazer
+      .setGazeListener(listener)
+      .setRegression('ridge') // 'weightedRidge' 'threadedRidge'
+      .begin()
+      .then(() => console.log('WebGazer initialized'))
+
+    return () => {
+      webgazer.clearGazeListener()
+    }
+  }, [])
   return (
     <div>
       <Sidebar />
